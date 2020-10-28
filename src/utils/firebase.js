8aa86601b3afafe0873e82eba7cc5b6aa86ad3c8
Joshua A. Díaz Robayna <66664769+jadrdev@users.noreply.github.com>
2020-10-28 15:45:07 +0000

    Conexión Firebase Con RN

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase/app";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAUw7JPdCUDU1n-cD6mW0n8YFGbXeVsSW8",
     authDomain: "chat-7ecbe.firebaseapp.com",
@@ -6,4 +8,7 @@ const firebaseConfig = {
     storageBucket: "chat-7ecbe.appspot.com",
     messagingSenderId: "263557150217",
     appId: "1:263557150217:web:8f760611c9a1f96e972ad8"
-}
+};
+
+  // Inicializamos y exportamos firebase
+   export default firebase.initializeApp(firebaseConfig);
